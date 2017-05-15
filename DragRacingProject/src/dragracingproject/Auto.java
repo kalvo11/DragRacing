6f801a169213ba1488b22ade41c1874c26f3c583
KSH <kelvin.schulz@orgadata.com>
2017-05-15 13:48:43 +0200

    Added Auto Class

---
+++ DragRacingProject/src/dragracingproject/Auto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dragracingproject;
+
+/**
+ *
+ * @author ksh
+ */
+public class Auto 
+{
+    public Auto()
+    {
+        
+    }
+}
